12d1aead53c935be29835ad6e026dbc6d7b12ed6
Leana <leananeparidze.com>
2019-01-21 23:17:08 -0800

    Displayes form on the page with the button to output ajax call response

---
+++ src/components/App.jsx
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import $ from 'jquery';
+
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+  onSubmitClick() {
+    var input = {
+        name: $('#name').val(),
+        message: $('#message').val()
+    }
+    console.log("clicked")
+        $.ajax({
+          url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf110/greeting',
+          type: 'POST',
+          data: JSON.stringify(input),
+          contentType: 'application/json',
+          success: function(response) {
+              this.append(response)
+            },
+          error: function (error) {
+            console.error(error);
+          }
+        });
+    
+  }
+
+  append(response) {
+      document.$('#response').append(response);
+  }
+
+    render() {
+      return (
+        <div>
+            <p>Server Response:</p>
+            <p id="response"></p>
+        <form>
+            Name: <input type="text" name="name" id="name"/>
+            Message: <input type="text" name="message" id="message"/>
+            <button className='button' onClick={this.onSubmitClick}>Submit</button>
+        </form>
+        </div>
+      )
+    }
+  }
+
+export default App;
---
+++ src/components/Append.jsx
@@ -0,0 +1,7 @@
+
+
+export class Append extends React.component {
+    makeRequest
+};
+
+export default Append;
