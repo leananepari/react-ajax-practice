b7c316587da43e4442d24237f820c4a2bdb1db60
Leana <leananeparidze.com>
2019-01-22 07:45:44 -0800

    Recieves and outputs response from the server on to the screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,29 @@ import $ from 'jquery';
 class App extends React.Component {
     constructor(props) {
       super(props);
+
+      this.state = {
+
+      }
     }
+
   
-  onSubmitClick() {
+  onSubmitClick(event) {
+    event.preventDefault();
+
     var input = {
         name: $('#name').val(),
         message: $('#message').val()
     }
     console.log("clicked")
+    console.log(input)
         $.ajax({
           url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf110/greeting',
           type: 'POST',
           data: JSON.stringify(input),
           contentType: 'application/json',
           success: function(response) {
-              this.append(response)
+            $('#response').append(response)
             },
           error: function (error) {
             console.error(error);
@@ -41,7 +49,7 @@ class App extends React.Component {
         <form>
             Name: <input type="text" name="name" id="name"/>
             Message: <input type="text" name="message" id="message"/>
-            <button className='button' onClick={this.onSubmitClick}>Submit</button>
+            <button className='button' onClick={this.onSubmitClick.bind(this)}>Submit</button>
         </form>
         </div>
       )
